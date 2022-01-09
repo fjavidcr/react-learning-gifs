cbd910915f7cc0aedff4d81fdd84015b843a8ee4
F. Javier del Castillo <javicastillo92@gmail.com>
2022-01-09 12:38:28 +0100

    fetching marvel gifs from GIPHY

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import { apiURL } from './constants';
+
+// const GIFS = [
+//   'https://media4.giphy.com/media/EPcvhM28ER9XW/giphy.gif?cid=790b761147f128d039ab8ad83a0dc481bd5d9f94865e9e52&rid=giphy.gif&ct=g',
+//   'https://media0.giphy.com/media/N6funLtVsHW0g/giphy.gif?cid=ecf05e47t76fbdzf5l9eowg5tu599q4f0thunkth38xyqp8g&rid=giphy.gif&ct=g'
+// ]
+
+// const OTHER_GIFS = [
+//   'https://media2.giphy.com/media/vBjLa5DQwwxbi/giphy.gif?cid=ecf05e47hw0c2evym17pzsvhj0wwvrbu6w0z9dm1v0dnef22&rid=giphy.gif&ct=g',
+//   'https://media2.giphy.com/media/rj12FejFUysTK/giphy.gif?cid=ecf05e47hw0c2evym17pzsvhj0wwvrbu6w0z9dm1v0dnef22&rid=giphy.gif&ct=g'
+// ]
+
 
 function App() {
+  // const [gifs, setGifs] = useState(GIFS)
+  const [gifs, setGifs] = useState([])
+
+  useEffect(function () {
+    console.log('efecto ejecutado');
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(response => {
+        const {data = []} = response // = [] es valor por defecto y response es undefined
+        const gifs = data.map(image => image.images.downsized_medium.url)
+        console.log(gifs)
+        setGifs(gifs)
+      })
+  }, []) // [] significa que no tiene dependencias y solo se ejecuta una vez
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="Gifs-search">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {
+          gifs.map( gif => <img src={gif} alt='gif-image' />)
+        }
+        {/* <button onClick={() => setGifs(OTHER_GIFS)}>Cambiar gifs</button> */}
+      </section>
     </div>
   );
 }
