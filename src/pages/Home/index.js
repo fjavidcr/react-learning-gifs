2ef138aafccbf01cd1cc00514f8743914ab9196d
F. Javier del Castillo <javicastillo92@gmail.com>
2022-01-14 00:04:26 +0100

    refactor home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,9 @@ import React, { useState } from "react"
 import { Link, useLocation } from 'wouter'
 import './Home.css'
 import GifsList from "../../components/GifsList"
+import LinkList from "../../components/LinkList"
 import { useGifs } from "../../hooks/useGifs"
+import TrendingSearches from "../../components/TrendingSearches"
 
 
 const POPULAR_GIFS = ["Marvel", "Ironman", "Capitan America", "Dr Strange"]
@@ -27,16 +29,8 @@ export default function Home () {
       <form onSubmit={handleSubmit}>
         <input onChange={handleChange} type='text' value={keyword} placeholder="Search a gif here ..."/>
       </form>
-      <h3 className="App-title">Los gifs más populares</h3>
-      <ul>
-        {
-          POPULAR_GIFS.map( gif => 
-            <li key={gif}>
-              <Link to={`/search/${gif}`}>Gifs de {gif}</Link>
-            </li>
-          )
-        }
-      </ul>
+      {/* <LinkList title='Los gifs más populares' items={POPULAR_GIFS}/> */}
+      <TrendingSearches />
       <h3 className="App-title">Última búsqueda realizada</h3>
       <section className="Gifs-search">
         <GifsList gifs={gifs} />
