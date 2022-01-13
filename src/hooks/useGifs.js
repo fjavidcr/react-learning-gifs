0553a5ce7778420934de92f3a61fa1bb568524d1
F. Javier del Castillo <javicastillo92@gmail.com>
2022-01-13 19:48:35 +0100

    set last keyword localstorage

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,20 +1,23 @@
 import { useEffect, useState } from 'react'
 import GhifyApi from '../services/GhipyApi'
 
-export function useGifs ({ keyword }) {
+export function useGifs ({ keyword } = { keyword: null }) {
   const [loading, setLoading] = useState(false)
   const [gifs, setGifs] = useState([])
 
   useEffect(function () {
     console.log('fetch gifs')
     setLoading(true)
+    
+    const keywordToUse = keyword || localStorage.getItem('lastKeyword')
 
     GhifyApi.getGifs({
-      keyword,
+      keyword: keywordToUse,
       limit: 12
     }).then( gifs => {
       setGifs(gifs)
       setLoading(false)
+      localStorage.setItem('lastKeyword', keyword)
     })
   }, [keyword]) // [] significa que no tiene dependencias y solo se ejecuta una vez
 
